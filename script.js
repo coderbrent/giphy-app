f389fd21cbe55fff796f8f4c4524467257948ab3
Brent Abruzese <brent.abruzese@gmail.com>
2019-03-27 17:55:14 -0400

    added functionality

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
 var APIkey = "nq5lkeEwDVExNdGEubv2uPOutyxK4U19";
-var searchQuery = "hobo";
 var searchLimiter = "5";
 var userRating = "R";
-var queryURL = "https://api.giphy.com/v1/gifs/search?q="+ searchQuery + "&api_key=" + APIkey + "&limit=" + searchLimiter + "&rating=" + userRating;
-var topics = ["nes games", "snes games", "sega games", "atari games", "turbografix 16 games", "jaguar 64 games", "neo geo games"];
+
+var topics = ["nintendo", "sega", "atari", "turbografix", "jaguar64", "neo geo"];
+var newBtn;
 
 
-$("#generate").on("click", function() {
-  for(i=0; i < topics.length; i++) {
-    var newBtn = $("<button>").addClass("m-2");
-    newBtn.attr("data-name", topics[i]);
-    $(newBtn).prependTo("#gif-feed");
-    $(newBtn).text(topics[i]);
-  }
+for(i=0; i < topics.length; i++) {
+  newBtn = $("<button>").addClass("m-2 newBtn");
+  newBtn.attr("data-name", topics[i]);
+  $(newBtn).prependTo("#button-feed");
+  $(newBtn).text(topics[i]);
+}
 
+$(document.body).on("click", ".newBtn", function() {
+  searchQuery = $(this).attr("data-name");
+  userRating = $(this).attr("")
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchQuery + "&api_key=" + APIkey + "&limit=" + searchLimiter + "&rating=" + userRating;
+  
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(data) { 
     console.log("success got data", data);
-    for(i=0; i < data.length; i++) {
-      console.log([i]);
-    }
+    var card = $("<img>");
+    let gif = data.data[0].images.original_mp4.mp4;
+    console.log(gif);
+    $(card).attr("src", gif);
+    $(card).appendTo("#gif-feed");
   });
 
-})
+});
+
 
 
 
